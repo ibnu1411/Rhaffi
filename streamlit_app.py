8bb5cae5d7c6673a0ff4cf1136090606554daeb6
ibnu1411 <rhaffigaming@gmail.com>
2025-05-21 13:54:34 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,4 @@ st.title("Fiixyn")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+st.image("IMG_20250513_102151.jpg", width=200)
